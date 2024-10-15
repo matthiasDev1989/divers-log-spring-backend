a4d4903506a2a3f5430e938f5852ec7a3eef4f49
matthiasDev1989 <matthiasDev@gmx.at>
2024-10-15 14:41:27 +0200

    Creates SpringBoot backend project for divers log

---
+++ src/main/java/mn/diverslogspringbackend/DiversLogSpringBackendApplication.java
@@ -0,0 +1,13 @@
+package mn.diverslogspringbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiversLogSpringBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiversLogSpringBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mn/diverslogspringbackend/DiversLogSpringBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package mn.diverslogspringbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiversLogSpringBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
